16618bb69b81adfd3abe5552bbe1e9669f385943
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-06-30 18:50:36 +0300

    Signal type Predicate

---
+++ Assets/Scripts/Core/Predicate/SignalPredicate.cs
@@ -0,0 +1,19 @@
+public class SignalPredicate : IPredicate
+{
+    bool isSignaled;
+
+    public void Fire() => isSignaled = true;
+
+    public void Reset() => isSignaled = false;
+
+    public bool Evaluate()
+    {
+        if (isSignaled)
+        {
+            isSignaled = false;
+            return true;
+        }
+
+        return false;
+    }
+}
